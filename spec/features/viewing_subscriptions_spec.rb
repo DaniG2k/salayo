23e30ceb690457aa41002423ad77dd86c70e3a10
DaniG2k <byakugan.87@gmail.com>
2017-08-01 22:04:42 +0900

    Added subscription spec

---
+++ spec/features/viewing_subscriptions_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Viewing subscriptions', type: :feature do
+  let(:admin) {FactoryGirl.create(:user, :admin)}
+  let!(:subscriber) {FactoryGirl.create(:subscription)}
+
+  before :each do
+    login_as :admin
+    visit subscriptions_path
+  end
+
+  it 'list subscribed emails' do
+    expect(page).to have_content(subscriber.email)
+  end
+end
