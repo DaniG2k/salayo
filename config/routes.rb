3e9381d5b130d290573ccf156be0fe237d2280b1
DaniG2k <byakugan.87@gmail.com>
2017-08-05 12:29:23 +0900

    Remove registration for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+  devise_for :users, skip: [:registrations], path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
   root 'welcome#index'
 
   get 'contact', to: 'contact_messages#new', as: 'new_message'
