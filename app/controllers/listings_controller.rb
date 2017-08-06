5a0babcf156740eb2f4e5aea4c37bb83d70b16e5
DaniG2k <byakugan.87@gmail.com>
2017-08-06 15:18:11 +0900

    Added listings model and controller

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,45 @@
+class ListingsController < ApplicationController
+  layout 'dashboard'
+  before_action :set_listing, only: [:show, :edit, :update]
+
+  def index
+    @listings = Listing.where(user_id: current_user.id)
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new listing_params
+
+    if @listing.save
+      flash[:notice] = 'Listing was successfully created!'
+      redirect_to listing_path(@listing)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    if @listing.destroy
+      redirect_to dashboard_path, notice: 'Listing was successfully destroyed!'
+    else
+      flash.now[:notice] = 'ArtiListingcle was not destroyed.'
+    end
+  end
+
+  private
+    def set_listing
+      @listing = Listing.find(params[:id])
+    end
+end
---
+++ spec/features/viewing_listings_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature 'Viewing Listings' do
+  let(:user) {FactoryGirl.create(:user)}
+  let(:listing) {FactoryGirl.create(:listing, owner: user)}
+
+  before do
+    visit '/login'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'supersecurep@ss'
+    click_button 'Log in'
+  end
+
+  it "shows the logged in user's listings" do
+    click_link 'My listings'
+
+    expect(page).to have_content(listing.name)
+  end
+end
