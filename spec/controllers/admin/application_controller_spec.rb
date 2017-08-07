aadbdfce7b7779ac17e7919d32d7bffe75baeed3
DaniG2k <byakugan.87@gmail.com>
2017-08-07 15:24:21 +0900

    Fix spec

--- spec/controllers/admin/application_controller_spec.rb
+++ spec/controllers/admin/application_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Admin::ApplicationController, type: :controller do
-  let(user) {FactoryGirl.create(:user)}
+  let(:user) {FactoryGirl.create(:user)}
 
   before do
     allow(controller).to receive(:authenticate_user!)
