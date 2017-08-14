c7e18c17b1356567543171951aa38da86c7862ec
DaniG2k <byakugan.87@gmail.com>
2017-08-14 20:27:49 +0900

    Added access control to Users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
   layout 'dashboard'
 
   def show
-    @user = User.find(params[:id])
+    authorize @user, :show?
   end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
