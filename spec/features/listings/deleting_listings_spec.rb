d9706534cd63d57b83d8970b859167f26a652e0b
DaniG2k <byakugan.87@gmail.com>
2017-08-15 12:28:53 +0900

    Fixed editing_listings_spec.rb

--- spec/features/listings/deleting_listings_spec.rb
+++ spec/features/listings/deleting_listings_spec.rb
@@ -7,14 +7,14 @@ RSpec.feature 'Listing owners can delete listings' do
   before do
     user.add_role(:owner, listing)
     login_as user
-    visit listings_path
+    visit listings_path(locale: user.locale)
   end
 
   scenario 'successfully' do
     click_link 'Delete'
 
     expect(page).to have_content 'Listing was successfully deleted!'
-    expect(page.current_url).to eq listings_url
+    expect(page.current_url).to eq listings_url(locale: user.locale)
     expect(page).not_to have_content listing.name
   end
 end
