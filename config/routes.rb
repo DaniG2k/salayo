905ede1aa9eab53218d49bd7d9827925f111e33e
DaniG2k <byakugan.87@gmail.com>
2017-08-15 11:51:27 +0900

    Adding locale scoping in url

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,29 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  scope "/:locale", locale: /en|ko/ do
+    root 'welcome#index'
+    devise_for(
+      :users,
+      path: '',
+      skip: [:registrations],
+      path_names: { sign_in: 'login', sign_out: 'logout' }
+    )
+    as :user do
+      get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
+      patch 'users' => 'devise/registrations#update', as: 'user_registration'
+    end
+    resources :users, only: [:show]
 
-  root 'welcome#index'
-  devise_for(
-    :users,
-    path: '',
-    skip: [:registrations],
-    path_names: { sign_in: 'login', sign_out: 'logout' }
-  )
-  as :user do
-    get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
-    patch 'users' => 'devise/registrations#update', as: 'user_registration'
+    get 'contact', to: 'contact_messages#new', as: 'new_message'
+    post 'contact', to: 'contact_messages#create', as: 'create_message'
+    post 'subscribe', to: 'welcome#subscribe', as: 'subscribe_email'
+    get 'dashboard', to: 'dashboard#index'
+    get 'search', to: 'dashboard#search'
+
+    namespace :admin do
+      root 'application#index'
+      get 'subscriptions', to: 'subscriptions#index'
+    end
+
+    resources :listings
   end
-  resources :users, only: [:show]
-
-  get 'contact', to: 'contact_messages#new', as: 'new_message'
-  post 'contact', to: 'contact_messages#create', as: 'create_message'
-  post 'subscribe', to: 'welcome#subscribe', as: 'subscribe_email'
-  get 'dashboard', to: 'dashboard#index'
-  get 'search', to: 'dashboard#search'
-
-  namespace :admin do
-    root 'application#index'
-    get 'subscriptions', to: 'subscriptions#index'
-  end
-
-  resources :listings
 end
