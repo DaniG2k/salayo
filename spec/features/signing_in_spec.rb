49c142e6b00955dac7790fc6ac2cd46a29120c10
DaniG2k <byakugan.87@gmail.com>
2017-08-15 13:03:52 +0900

    Localizing...

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature 'Registered users can sign in' do
-  let!(:user) {FactoryGirl.create(:user)}
+  let!(:user) {FactoryGirl.create(:user, locale: 'en')}
 
   scenario 'with valid credentials' do
     visit '/en/login'
     fill_in 'Email', with: user.email
     fill_in 'Password', with: 'supersecurep@ss'
-    click_button 'Log in'
+    click_button 'Sign in'
 
     expect(page).to have_content 'Signed in successfully'
     expect(current_path).to eq dashboard_path(locale: 'en')
