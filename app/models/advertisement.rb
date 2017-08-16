d86b9a5009e422f1e606e1ec0622f885a10d0a1d
DaniG2k <byakugan.87@gmail.com>
2017-08-17 01:31:27 +0900

    Add colored badges to advertisements

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,5 +1,8 @@
 class Advertisement < ApplicationRecord
+  ALLOWED_TYPES = %w(rent let wanted).freeze
+
   validates :user, :title, :body, :ad_type, presence: true
+  validates :ad_type, inclusion: { in: ALLOWED_TYPES }
 
   belongs_to :user
 end
