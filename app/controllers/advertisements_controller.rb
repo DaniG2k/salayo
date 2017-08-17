7b93b34f8a5ea644b716947df47779542398825e
DaniG2k <byakugan.87@gmail.com>
2017-08-17 14:28:49 +0900

    Added create advertisements feature

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -7,62 +7,42 @@ class AdvertisementsController < ApplicationController
     @advertisements = Advertisement.all.includes(:user)
   end
 
-  # GET /advertisements/1
-  # GET /advertisements/1.json
   def show
   end
 
-  # GET /advertisements/new
   def new
     @advertisement = Advertisement.new
   end
 
-  # GET /advertisements/1/edit
   def edit
   end
 
-  # POST /advertisements
-  # POST /advertisements.json
   def create
     @advertisement = Advertisement.new(advertisement_params)
+    @advertisement.user = current_user
 
-    respond_to do |format|
-      if @advertisement.save
-        format.html { redirect_to @advertisement, notice: 'Advertisement was successfully created.' }
-        format.json { render :show, status: :created, location: @advertisement }
-      else
-        format.html { render :new }
-        format.json { render json: @advertisement.errors, status: :unprocessable_entity }
-      end
+    if @advertisement.save
+      redirect_to @advertisement, notice: 'Advertisement was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /advertisements/1
-  # PATCH/PUT /advertisements/1.json
   def update
-    respond_to do |format|
-      if @advertisement.update(advertisement_params)
-        format.html { redirect_to @advertisement, notice: 'Advertisement was successfully updated.' }
-        format.json { render :show, status: :ok, location: @advertisement }
-      else
-        format.html { render :edit }
-        format.json { render json: @advertisement.errors, status: :unprocessable_entity }
-      end
+    if @advertisement.update(advertisement_params)
+      redirect_to @advertisement, notice: 'Advertisement was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /advertisements/1
-  # DELETE /advertisements/1.json
   def destroy
     @advertisement.destroy
-    respond_to do |format|
-      format.html { redirect_to advertisements_url, notice: 'Advertisement was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to advertisements_url, notice: 'Advertisement was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_advertisement
       @advertisement = Advertisement.find(params[:id])
     rescue ActiveRecord::RecordNotFound
@@ -70,8 +50,7 @@ class AdvertisementsController < ApplicationController
       redirect_to advertisements_path
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def advertisement_params
-      params.fetch(:advertisement, {})
+      params.require(:advertisement).permit(:title, :body, :ad_type)
     end
 end
