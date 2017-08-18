f1feee9559a5415866c7bdf97ef566635657caf5
DaniG2k <byakugan.87@gmail.com>
2017-08-19 02:10:08 +0900

    Building chat

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,28 +3,21 @@ class ChatroomsController < ApplicationController
   before_action :set_chatroom, only: [:show, :edit, :update, :destroy]
   layout 'dashboard'
 
-  # GET /chatrooms
-  # GET /chatrooms.json
   def index
     @chatrooms = Chatroom.all
   end
 
-  # GET /chatrooms/1
-  # GET /chatrooms/1.json
   def show
+    @messages = @chatroom.messages.includes(:user).order(created_at: :desc).reverse
   end
 
-  # GET /chatrooms/new
   def new
     @chatroom = Chatroom.new
   end
 
-  # GET /chatrooms/1/edit
   def edit
   end
 
-  # POST /chatrooms
-  # POST /chatrooms.json
   def create
     @chatroom = Chatroom.new(chatroom_params)
 
@@ -39,8 +32,6 @@ class ChatroomsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /chatrooms/1
-  # PATCH/PUT /chatrooms/1.json
   def update
     respond_to do |format|
       if @chatroom.update(chatroom_params)
@@ -53,8 +44,6 @@ class ChatroomsController < ApplicationController
     end
   end
 
-  # DELETE /chatrooms/1
-  # DELETE /chatrooms/1.json
   def destroy
     @chatroom.destroy
     respond_to do |format|
@@ -68,12 +57,11 @@ class ChatroomsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_chatroom
       @chatroom = Chatroom.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def chatroom_params
       params.require(:chatroom).permit(:name)
     end
