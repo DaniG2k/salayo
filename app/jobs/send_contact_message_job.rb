2ab4acfe9248c1363a975ac612530bb16678b6c0
DaniG2k <byakugan.87@gmail.com>
2017-08-23 17:55:12 +0900

    Got emailing to work

--- app/jobs/send_contact_message_job.rb
+++ app/jobs/send_contact_message_job.rb
@@ -2,6 +2,6 @@ class SendContactMessageJob < ApplicationJob
   queue_as :low
 
   def perform(msg)
-    ContactMessageMailer.dispatch(msg).deliver!#.deliver_later
+    ContactMessageMailer.dispatch(msg).deliver!
   end
 end
